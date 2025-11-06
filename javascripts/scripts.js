327ff33dadcbc74010734048dce8fabaee18c6b2
Allegra Anka <allegra@fullstory.com>
2025-11-05 22:10:04 -0500

    added new FS functions, identify, anonymize

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,6 +1,8 @@
 const sessionUrlInjection = document.getElementById("sessionURL-injection");
 const sessionUrlLabel = document.getElementById("sessionURL-label");
 const startCapture = document.getElementById("startCapture");
+const anonymize = document.getElementById("anonymize");
+const identify = document.getElementById("identify");
 const shutdownCapture = document.getElementById("shutdownCapture");
 const orgId = document.getElementById("displayOrgId");
 
@@ -15,10 +17,10 @@ FS("observe", {
       console.log(sessionURL);
       sessionUrlLabel.innerHTML = '';
       sessionUrlInjection.href = sessionURL;
-      sessionUrlInjection.innerHTML = "Session URL";
+      sessionUrlInjection.innerHTML = "Current session URL";
     } else {
-      console.log('Not capturing');
-      sessionUrlLabel.innerHTML = 'No session URL, not capturing.'; 
+      console.log('No session URL, not capturing');
+      sessionUrlLabel.innerHTML = "No session URL, not capturing."; 
     }
   },
 });
@@ -33,8 +35,8 @@ FS("observe", {
       sessionUrlInjection.href = sessionURL;
       sessionUrlInjection.innerHTML = "Session URL";
     } else {
-      console.log("Not capturing.");
-      sessionUrlLabel.innerHTML = "No session URL, not capturing.";
+      console.log("Called FS('shutdown'), not capturing.");
+      sessionUrlLabel.innerHTML = "Called FS('shutdown'). No session URL, not capturing.";
       sessionUrlInjection.innerHTML = "";
     }
     // clear browser cookies
@@ -47,10 +49,48 @@ startCapture.addEventListener('click', () => {
   FS('start');
 });
 
+// Anonymize, release current user identity and start new anonymous session
+anonymize.addEventListener('click', () => {
+  FS('setIdentity', { anonymous: true });
+});
+
 // Shutdown Fullstory Data Capture
 shutdownCapture.addEventListener('click', () => {
   FS('shutdown');
-})
+});
+
+// Identify current user
+identify.addEventListener('click', () => {
+  let uid = generateUid(15);
+  console.log(`Current uid is: ${uid}`);
+  FS('setIdentity', {
+    uid: uid,
+    properties: {
+      identifiedAt: Date.now()
+    }
+  });
+});
+
+// Helper code, generate random alphanumeric value of a given length
+function generateUid(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+// Fullstory ragehook
+window.addEventListener('fullstory/rageclick', (e) => {
+    console.log(`eventStartTimeStamp: ${e.detail.eventStartTimeStamp}`);
+    console.log(`eventEndTimeStamp: ${e.detail.eventEndTimeStamp}`);
+    console.log(`eventReplayUrlAtCurrentTime: ${e.detail.eventReplayUrlAtCurrentTime}`);
+    console.log(`eventReplayUrlAtStart: ${e.detail.eventReplayUrlAtStart}`);
+    console.log(`targetElement: ${e.target}`);
+    window.alert('Need some help there bud?');
+});
 
 // function deleteCookie(name, path = "/", domain = "") {
 //   console.log('Cookie name: ' + name, 'Cookie path: ' + path, 'Cookie domain: ' + domain);
